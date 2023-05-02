d878b71c5e812fbf923391a2022dddfb63d2d0d2
Vinicius Sutil <vinicius.sutilvs34@outlook.com>
2023-05-02 13:46:37 -0300

    Project created

---
+++ src/main/java/com/viniciussutil/webservicesproject/WebservicesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.viniciussutil.webservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciussutil/webservicesproject/WebservicesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciussutil.webservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
