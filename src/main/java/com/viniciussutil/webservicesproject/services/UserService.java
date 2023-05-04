ae72ae71d939d6a6493c36c9d050771bda3fe9e4
Vinicius Sutil <vinicius.sutilvs34@outlook.com>
2023-05-04 16:33:27 -0300

    Exception handling - findById

--- src/main/java/com/viniciussutil/webservicesproject/services/UserService.java
+++ src/main/java/com/viniciussutil/webservicesproject/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.viniciussutil.webservicesproject.entities.User;
 import com.viniciussutil.webservicesproject.repositories.UserRepository;
+import com.viniciussutil.webservicesproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
