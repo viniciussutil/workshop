98e3e2dce0d066c6e06231e28d1906b7bd66c237
Vinicius Sutil <vinicius.sutilvs34@outlook.com>
2023-05-04 10:22:47 -0300

    Payment, one-to-one association

--- src/main/java/com/viniciussutil/webservicesproject/config/TestConfig.java
+++ src/main/java/com/viniciussutil/webservicesproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniciussutil.webservicesproject.entities.Category;
 import com.viniciussutil.webservicesproject.entities.Order;
 import com.viniciussutil.webservicesproject.entities.OrderItem;
+import com.viniciussutil.webservicesproject.entities.Payment;
 import com.viniciussutil.webservicesproject.entities.Product;
 import com.viniciussutil.webservicesproject.entities.User;
 import com.viniciussutil.webservicesproject.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment  pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
